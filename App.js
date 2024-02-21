1e20625fc8ea6c736d356128053d716e2841dc19
newbi94 <newbi94@gmail.com>
2024-02-21 23:19:23 +0900

    24.02.21
    extraploate,
    parallel,
    rotateZ != rotate?,
    box-shadow in styled,

--- App.js
+++ App.js
@@ -1,103 +1,115 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useRef, useState } from 'react';
 import { 
+  View,
   Animated, 
   Dimensions,
   PanResponder,
 } from 'react-native';
 import styled from 'styled-components/native';
+import { Ionicons } from "@expo/vector-icons";
+
+
 
 const Container = styled.View`
   flex:1;
   justify-content: center;
   align-items: center;
-`;
-const Box = styled.View`
-  background-color: tomato;
-  width: 200px;
-  height: 200px;
+  background-color: #00a8ff;
 `;
 
-const AnimatedBox = Animated.createAnimatedComponent(Box)
+const Card = styled(Animated.createAnimatedComponent(View))`
+  background-color: white;
+  width: 300px;
+  height: 300px;
+  justify-content: center;
+  align-items: center;
+  border-radius: 12px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+`;
+//box-shadow가 입체감을 불어 넣는다.
+
+/* const Card = styled.View``;
+const AnimatedCard = Animated.createAnimatedComponent(Card); 
+이렇게 구성하면 아래 return 파트에서 태그 내에 Animated관련 자동완성기능이
+작동하지 않는다. 저번엔 니꼬가 이게 좋다더니 이제와서 바꾸다니
+*/
 
 export default function App() {
+  const scale = useRef(new Animated.Value(1)).current; 
+  //scale은 아래 <Card 내부에 style={{~ 에서 쓰이는 프로퍼티다. 프로퍼티 자체를 정의 해준 것.
+  //때문에 Animated.Value(1)에서 1은 스케일의 기본 값을 뜻한다.
+  const position = useRef(new Animated.Value(0)).current;
+  //position에서의 Animated.Value(0) 0은 아래 translateX에 쓰여있으니 Card의 x좌표 위치 기본 값이 되겠다.
+  const rotation = position.interpolate({
+    inputRange:[-250, 250],
+    outputRange:["-15deg", "15deg"],
+    extrapolate: "clamp"
+    // inputRange를 넘어서면 어떻게 할 것인지 설정하는 프로퍼티.
+    //"clamp"는 input이 넘어서도 해당 outputRange 끝 값에서 멈춘다.
+    //"extend"는 input과 output의 비율에 맞춰 계속해서 진행된다.
+    //"identity"는 넘어서면 이상해진다(?).
+  });
+  const onPressIn = () => 
+    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
+  const onPressOut =
+    Animated.spring(scale, { toValue: 1, useNativeDriver: true });
+  const goCenter = 
+    Animated.spring(position, { toValue: 0, useNativeDriver: true,});
   
-  const POSITION = useRef(
-    new Animated.ValueXY({
-      x: 0,
-      y: 0,
-    })
-  ).current;
-
-  const borderRadius = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: [100, 0],
-  });
-  const bgColor = POSITION.y.interpolate({
-    inputRange: [-300, 300],
-    outputRange: ["rgb(255, 99, 71)", "rgb(71, 166, 255)"],
-  });
+  //onPressIn처럼 끝에 .start()를 붙이고 onPanResponderGrant: () => onPressIn(),
+  //이런식으로 해도 되지만 onPressOut과 goCenter처럼 끝에 ()=> 와 .start()를 지우고 
+  //release부분에서 Animated.parallel([onPressOut, goCenter]).start();
+  //이렇게 불러 사용하는 방법도 있다.
+  
   const panResponder = useRef(
     PanResponder.create({
-      onStartShouldSetPanResponder: () => true, 
-      onPanResponderGrant: () => { //터치가 시작될 때 호출되는 함수
-        console.log("Touch Started");
-        POSITION.setOffset({
-          x: POSITION.x._value,
-          y: POSITION.y._value,
-        })
-        console.log(POSITION.x._value,POSITION.y._value)
-      },
-        //x: POSITON.x라고 해주면 number가 아니라는 에러가 뜬다.
-        //POSITON.x는 여러 함수가 포함된 Animated.Value를 받아오는 것.
-        //setOffset은 아래 onPanResponderMove의 POSITION.setValue를 통한 드래그를 구현하는 과정 중
-        //터치 오프후 다시 터치시 중앙(0의 위치)에서 시작되는 것을 방지하고 터치 오프할 때의 위치에서
-        //계속해서 드래그를 진행하기 위해 사용하는 메소드이다.
-        //onPanResponderMove의 dx,dy값은 말 그대로 터치후부터 터치오프까지의 '거리' 이기 때문에
-        //터치 오프후 다시 터치를 하면 dx,dy값은 0부터 시작하여 POSITION.setValue를 통해 x와 y를 0으로 즉시 변경,
-        //박스가 중앙으로 순간이동한후 드래그된다.
-        //setOffset은 터치가 시작할 때 0부터 시작하지 않고 전의 좌표 값을 적용시켜 (전의 좌표 값) + (터치후 이동거리)
-        //로 계산하여 이어서 드래그할 수 있게 해준다.
-      
-      onPanResponderMove:(_, { dx ,dy }) =>{ //터치 중일 때 호출되는 함수
-        console.log("Finger Moving");
-        POSITION.setValue({
-          x: dx,
-          y: dy,
-        })
-      },
-      onPanResponderRelease: () => { //터치가 끝났을 때 호출되는 함수
-        console.log("Touch Finished");
-        POSITION.flattenOffset();
-      }
-      //flattenOffset() Offset의 값이 누적되는 것을 막기위한 메소드. 값을 0으로 비워준다.
+    onStartShouldSetPanResponder: () => true,
 
-      /*  onPanResponderRelease: () => {
-        Animated.spring(POSITION,{
-          toValue: {
-            x:0,
-            y:0,
-          },
-          bounciness: 20,
-          useNativeDriver: false,
-        }).start(); 
-        드래그후 손을 뗐을 때 중앙으로 돌아가는 애니메이션
-        */
-    })
+    onPanResponderMove: (_, {dx}) => {
+      position.setValue(dx);
+    },
+
+    onPanResponderGrant: () => onPressIn(),
+
+    onPanResponderRelease: (_, { dx }) => {
+      if (dx < -300) {
+        Animated.spring(position, {
+          toValue: -500,
+          useNativeDriver: true,
+        }).start();
+      } else if (dx > 300) {
+        Animated.spring(position, {
+          toValue: 500,
+          useNativeDriver: true,
+        }).start();
+      } else {
+      Animated.parallel([ onPressOut, goCenter ]).start();
+    }
+    console.log(dx)
+    },
+  })
   ).current;
-  
+  //x좌표가 300 또는 -300을 넘어서면 toValue를 화면 이상의 값으로 설정해
+  //날려버리는 애니메이션을 구현했다.
+  //Animated.parallel을 통해 여러 애니메이션을 묶어서 실행한다.
     return (
       <Container>
-          <AnimatedBox
+        <Card 
           {...panResponder.panHandlers}
-            style={{
-              borderRadius,
-              backgroundColor: bgColor,
-              transform: [...POSITION.getTranslateTransform()],
-            }}
-          />
+          style={{
+            transform: [
+              { scale },
+              {translateX: position},
+              {rotateZ: rotation},
+            ],
+          }}
+        >
+          <Ionicons name="pizza" color="#192a56" size={98} />
+        </Card>
       </Container>
     );
   }
+//rotateZ가 아니라 그냥 rotate라고 해도 똑같이 작동하는 것으로 보인다. 무슨 차이인지는 모르겠다.
 
   
