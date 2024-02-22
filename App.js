03baf9bf404001641a21151939d6193b2a0f2561
newbi94 <newbi94@gmail.com>
2024-02-22 15:41:45 +0900

    24.02.22
    restDisplacementThreshold,
    restSpeedThreshold,
    ~.start( fn ),

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 import styled from 'styled-components/native';
 import { Ionicons } from "@expo/vector-icons";
+import icons from './icons';
 
 
 
@@ -26,8 +27,11 @@ const Card = styled(Animated.createAnimatedComponent(View))`
   align-items: center;
   border-radius: 12px;
   box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.2);
+  position: absolute;
 `;
 //box-shadow가 입체감을 불어 넣는다.
+//position: absolute는 모든 Card의 위치를 똑같이 위치시킨다.
+//즉 겹치게 된다.
 
 /* const Card = styled.View``;
 const AnimatedCard = Animated.createAnimatedComponent(Card); 
@@ -35,66 +39,114 @@ const AnimatedCard = Animated.createAnimatedComponent(Card);
 작동하지 않는다. 저번엔 니꼬가 이게 좋다더니 이제와서 바꾸다니
 */
 
+const Btn = styled.TouchableOpacity`
+  margin: 0px 10px;
+`;
+
+const BtnContainer = styled.View`
+  flex-direction: row;
+  flex: 1;
+`;
+
+const CardContainer = styled.View`
+  flex:3;
+  justify-content: center;
+  align-items: center;
+`;
+
+
 export default function App() {
+  
   const scale = useRef(new Animated.Value(1)).current; 
-  //scale은 아래 <Card 내부에 style={{~ 에서 쓰이는 프로퍼티다. 프로퍼티 자체를 정의 해준 것.
-  //때문에 Animated.Value(1)에서 1은 스케일의 기본 값을 뜻한다.
+
   const position = useRef(new Animated.Value(0)).current;
-  //position에서의 Animated.Value(0) 0은 아래 translateX에 쓰여있으니 Card의 x좌표 위치 기본 값이 되겠다.
+  
   const rotation = position.interpolate({
     inputRange:[-250, 250],
     outputRange:["-15deg", "15deg"],
     extrapolate: "clamp"
-    // inputRange를 넘어서면 어떻게 할 것인지 설정하는 프로퍼티.
-    //"clamp"는 input이 넘어서도 해당 outputRange 끝 값에서 멈춘다.
-    //"extend"는 input과 output의 비율에 맞춰 계속해서 진행된다.
-    //"identity"는 넘어서면 이상해진다(?).
   });
-  const onPressIn = () => 
-    Animated.spring(scale, { toValue: 0.95, useNativeDriver: true }).start();
-  const onPressOut =
-    Animated.spring(scale, { toValue: 1, useNativeDriver: true });
-  const goCenter = 
-    Animated.spring(position, { toValue: 0, useNativeDriver: true,});
-  
-  //onPressIn처럼 끝에 .start()를 붙이고 onPanResponderGrant: () => onPressIn(),
-  //이런식으로 해도 되지만 onPressOut과 goCenter처럼 끝에 ()=> 와 .start()를 지우고 
-  //release부분에서 Animated.parallel([onPressOut, goCenter]).start();
-  //이렇게 불러 사용하는 방법도 있다.
+  const secondScale = position.interpolate({
+    inputRange: [-300, 0 , 300],
+    outputRange: [1, 0.7 , 1],
+    extrapolate: "clamp",
+  })
+
+  const onPressIn = Animated.spring(scale, { 
+      toValue: 0.95, 
+      useNativeDriver: true 
+  });
+  const onPressOut = Animated.spring(scale, { 
+    toValue: 1, 
+    useNativeDriver: true 
+  });
+  const goCenter = Animated.spring(position, { 
+    toValue: 0, 
+    useNativeDriver: true,
+  });
+  const goLeft = Animated.spring(position, { 
+    toValue: -500, 
+    tension: 5, 
+    useNativeDriver: true, 
+    restDisplacementThreshold: 150,
+    restSpeedThreshold: 1000,
+  })
+  const goRight = Animated.spring(position, { 
+    toValue: 500, 
+    tension: 5, 
+    useNativeDriver: true,
+    restDisplacementThreshold: 150, //남은 거리에 따라 애니메이션을 끝낸다.
+    restSpeedThreshold: 1000, //남은 속도에 따라 애니메이션을 끝낸다.
+  })
+  //spring 애니메이션 특성상 애니메이션이 끝나기까지 시간이 꽤 걸린다.
+  //육안으로 끝나 보여도 진동이 1~3초간 남아 있는데, 아래 onDismiss함수는
+  //이 spring의 애니메이션이 완전히 끝나야 실행되기 때문에 카드를 넘기고
+  //즉시 바로 또 넘길 수가 없는 것.
+  //rest~Threshold 프로퍼티들은 그러한 것들을 속도나, 거리면에서 조정해준다.
+  const closePress = () => goLeft.start(onDismiss);
+  const checkPress = () => goRight.start(onDismiss);
   
   const panResponder = useRef(
     PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
+      onStartShouldSetPanResponder: () => true,
 
-    onPanResponderMove: (_, {dx}) => {
-      position.setValue(dx);
-    },
+      onPanResponderMove: (_, {dx}) => {
+        position.setValue(dx);
+      },
 
-    onPanResponderGrant: () => onPressIn(),
+      onPanResponderGrant: () => onPressIn.start(),
 
-    onPanResponderRelease: (_, { dx }) => {
-      if (dx < -300) {
-        Animated.spring(position, {
-          toValue: -500,
-          useNativeDriver: true,
-        }).start();
-      } else if (dx > 300) {
-        Animated.spring(position, {
-          toValue: 500,
-          useNativeDriver: true,
-        }).start();
-      } else {
-      Animated.parallel([ onPressOut, goCenter ]).start();
-    }
-    console.log(dx)
-    },
-  })
+      onPanResponderRelease: (_, { dx }) => {
+        if (dx < -250) {
+          goLeft.start(onDismiss);
+        } else if (dx > 250) {
+          goRight.start(onDismiss);
+        } else {
+        Animated.parallel([ onPressOut, goCenter ]).start();
+        }
+      },
+    })
   ).current;
-  //x좌표가 300 또는 -300을 넘어서면 toValue를 화면 이상의 값으로 설정해
-  //날려버리는 애니메이션을 구현했다.
-  //Animated.parallel을 통해 여러 애니메이션을 묶어서 실행한다.
+  const [index, setIndex] = useState(0);
+
+  const onDismiss = () => {
+    setIndex((prev) => prev + 1);
+    position.setValue(0);
+    scale.setValue(1);
+  }
+
     return (
       <Container>
+        <CardContainer>
+        <Card 
+          style={{
+            transform: [
+              { scale: secondScale },
+            ],
+          }}
+        >
+          <Ionicons name={icons[index+1]} color="#192a56" size={98} />
+        </Card>
         <Card 
           {...panResponder.panHandlers}
           style={{
@@ -105,11 +157,19 @@ export default function App() {
             ],
           }}
         >
-          <Ionicons name="pizza" color="#192a56" size={98} />
+          <Ionicons name={icons[index]} color="#192a56" size={98} />
         </Card>
+        </CardContainer>
+        <BtnContainer>
+          <Btn onPress={closePress}>
+            <Ionicons name="close-circle" color="white" size={58} />
+          </Btn>
+          <Btn onPress={checkPress}>
+            <Ionicons name="checkmark-circle" color="white" size={58} />
+          </Btn>
+        </BtnContainer>
       </Container>
     );
   }
-//rotateZ가 아니라 그냥 rotate라고 해도 똑같이 작동하는 것으로 보인다. 무슨 차이인지는 모르겠다.
 
   
